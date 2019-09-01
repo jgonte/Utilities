3ddffd74f93f159a236f2e19b7fdd87be5cb27a2
jgonte <jgonte@gmail.com>
2019-09-01 17:41:14 -0400

    Added "None" element to the command operations

--- Utilities/CommandOperations.cs
+++ Utilities/CommandOperations.cs
@@ -2,13 +2,14 @@
 {
     public enum CommandOperations
     {
-        Save = 1, // Create or update depending whether the identifier is null or not
-        Create = 2,
-        Update = 3,
-        Delete = 4,
-        DeleteCollection = 5,
-        AddLinkedEntity = 6,
-        ReplaceLinkedEntity = 7,
-        AddValueObjects = 8
+        None,
+        Save, // Create or update depending whether the identifier is null or not
+        Create,
+        Update,
+        Delete,
+        DeleteCollection,
+        AddLinkedEntity,
+        ReplaceLinkedEntity,
+        AddValueObjects
     }
 }
