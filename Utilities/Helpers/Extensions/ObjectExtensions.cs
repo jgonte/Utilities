1f942fbdb0ad8dc8c537fd5f7f47e6926d664f34
jgonte <jgonte@gmail.com>
2020-06-20 16:41:31 -0400

    Added an extension to get the key-value dictionary of an object

---
+++ Utilities/Helpers/Extensions/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Utilities
+{
+    public static class ObjectExtensions
+    {
+        public static IDictionary<string, object> ToDictionary(this object o)
+        {
+            var dictionary = new Dictionary<string, object>();
+
+            var typeAccessor = o.GetTypeAccessor();
+
+            foreach (var propertyAccessor in typeAccessor.PropertyAccessors)
+            {
+                dictionary.Add(propertyAccessor.Key, typeAccessor.GetValue(o, propertyAccessor.Key));
+            };
+
+            return dictionary;
+        }
+    }
+}
